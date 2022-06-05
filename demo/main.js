0a1f222bc87cb5dcfba2bdabadbd192f80207b71
Zamba Izo <ofuzak@gmail.com>
2022-06-06 02:33:32 +0300

    update version

--- demo/main.js
+++ demo/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import { createI18n } from "vue-i18n";
 import { createWeb3 } from '@/createWeb3';
 const infuraKey = process.env.VUE_APP_INFURA_KEY;
-import './assets/tailwind.css'
 import { theme } from './theme';
 import { providerOptions }  from './providers'
 const web3vue = createWeb3({
@@ -11,9 +10,7 @@ const web3vue = createWeb3({
     theme,
     global:true
 })
-import DemoApp from './App.vue';
-import translations from './messages'; // translations
-// 2. Create i18n instance with options
+
 const i18n = createI18n({
     locale: 'en', // set locale
     fallbackLocale: 'en', // set fallback locale
